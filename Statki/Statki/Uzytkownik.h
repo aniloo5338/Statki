e736dd54dc875a694b6bb49bfa6f935fde672d3a
aniloo5338 <96469861+aniloo5338@users.noreply.github.com>
2024-06-01 22:34:19 +0200

    Update Uzytkownik.h
    Oczyszczenie kodu (usunięcie zbędnych komentarzy, pustych linii)

--- Statki/Statki/Uzytkownik.h
+++ Statki/Statki/Uzytkownik.h
@@ -9,12 +9,7 @@ class Gracz {
 	string nick;
 	int wynik;
 public:
-	// deklaracje get'erow
-	//string get_nick() { return nick; }
 	int get_wynik() { return wynik; }
-	// deklaracje set'erow
-	/////////////////////////////////nie dziala pusty nick "anonim"////////////////////////////////
 	void set_nick() { if (nick == "\n") nick = "anonim"; }
-
 	string wpisz_nick();
 };
